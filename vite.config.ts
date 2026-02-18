0b588fba4b9c453c7d8814a74b8f6ade279a2215
Gunwoo1993 <jglabs.director@gmail.com>
2026-02-18 21:23:11 +0700

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,22 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    extensions: ['.mjs', '.js', '.mts', '.ts', '.jsx', '.tsx', '.json']
+  },
   build: {
     outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'lucide-react', '@supabase/supabase-js']
+        }
+      }
+    }
   },
   server: {
     port: 3000,
